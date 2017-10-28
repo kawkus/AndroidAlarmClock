6767c90169b2db72f0b72d660b9029f0cebe6d5f
kawkus27 <kawkus273@gmail.com>
2017-10-29 00:38:21 +0200

    Updating all files

---
+++ app/src/main/java/com/example/uytkownik/alarm/Main_activity.java
@@ -0,0 +1,13 @@
+package com.example.uytkownik.alarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity);
+    }
+}
---
+++ app/src/main/java/playSound.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Użytkownik on 2017-10-29.
+ */
+
+public class playSound {
+
+}
