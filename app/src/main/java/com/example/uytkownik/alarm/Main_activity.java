32a45004bcb12786e55479c580fbc282581c85e5
kawkus27 <kawkus273@gmail.com>
2018-02-18 19:48:48 +0100

    New Upgrade.

--- app/src/main/java/com/example/uytkownik/alarm/Main_activity.java
+++ app/src/main/java/com/example/uytkownik/alarm/Main_activity.java
@@ -1,7 +1,9 @@
 package com.example.uytkownik.alarm;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main_activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Main_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity);
+
+
+    }
+
+    public void playSound(View view) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.kbzb);
+        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+
     }
 }
--- app/src/main/java/com/example/uytkownik/alarm/playSound.java
+++ app/src/main/java/com/example/uytkownik/alarm/playSound.java
@@ -1,19 +1,42 @@
+package com.example.uytkownik.alarm;
+
+import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.IBinder;
 
-import com.example.uytkownik.alarm.Main_activity;
+
 import com.example.uytkownik.alarm.R;
 
 import static com.example.uytkownik.alarm.R.raw.kbzb;
 
-public class playSound {
+public class playSound  extends Service implements MediaPlayer.OnPreparedListener {
 
     private String soundName;
-    final static private int seconds;
+    final static private int seconds = 10;
+    private static final String ACTION_PLAY = "com.example.action.PLAY";
+    MediaPlayer mMediaPlayer = null;
 
-    static {
-        seconds = 10;
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if (intent.getAction().equals(ACTION_PLAY)) {
+
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+        }
+        return Service.START_STICKY_COMPATIBILITY;
     }
+
+    /** Called when MediaPlayer is ready */
+    public void onPrepared(MediaPlayer player) {
+        player.start();
+    }
+    public IBinder onBind(Intent intent)
+    {
+       return null;
+    }
+
+
 
     public playSound(String soundName) {
         this.soundName = soundName;
@@ -34,9 +57,8 @@ public class playSound {
 
     public void playSoundTheme ()
     {
-        MediaPlayer mediaPlayer;
-        mediaPlayer = MediaPlayer.create(this, kbzb);
-        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+        //MediaPlayer mediaPlayer = MediaPlayer.create(context, kbzb);
+       // mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 
 }
